56685590111a2b446c279dbb34d89b7efe9994ba
AdFiFi <1941761501@qq.com>
2022-12-02 11:54:22 +0800

    Create Utils

---
+++ Transformer/utils/masking.py
@@ -0,0 +1,21 @@
+import torch
+UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
+special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
+
+
+def create_mask(src, tgt, device):
+    src_padding_mask = (src == PAD_IDX).transpose(0, 1).to(device)
+    tgt_padding_mask = (tgt == PAD_IDX).transpose(0, 1).to(device)
+    return src_padding_mask, tgt_padding_mask
+
+
+class TriangularCausalMask():
+    def __init__(self, B, L, device="cpu"):
+        mask_shape = [B, 1, L, L]
+        with torch.no_grad():
+            self._mask = torch.triu(torch.ones(mask_shape, dtype=torch.bool), diagonal=1).to(device)
+
+    @property
+    def mask(self):
+        return self._mask
+
